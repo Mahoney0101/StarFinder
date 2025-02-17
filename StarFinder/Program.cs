9c0f56780b88417bfb3327a8ec2e7060e491c5e0
mahoney0101 <mahoneyjames67@gmail.com>
2025-02-17 22:35:49 +0000

    optimise

--- StarFinder/Program.cs
+++ StarFinder/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System.Globalization;
 
 if (args.Length != 5)
+{
     Console.WriteLine("Usage: dotnet run <filePath> <xCoord> <yCoord> <zCoord> <starsToReturn>");
-
+    return;
+}
 
 ProcessStarFinder(args);
 
@@ -25,14 +27,14 @@ static void ProcessStarFinder(string[] args)
     }
 }
 
-public readonly record struct StarData(double Distance, string StarName, double X, double Y, double Z);
+public readonly record struct StarData(double SquaredDistance, string StarName, double X, double Y, double Z);
 
 public static class StarFinder
 {
     public static List<(string StarName, double X, double Y, double Z)> FindClosestStars(
         string filePath, double xCoord, double yCoord, double zCoord, int starsToReturn)
     {
-        var maxHeap = new SortedSet<StarData>(new StarComparer());
+        var maxHeap = new PriorityQueue<StarData, double>();
 
         using var reader = new StreamReader(filePath);
         while (reader.ReadLine() is { } line)
@@ -41,39 +43,38 @@ public static class StarFinder
             if (parts.Length != 4) continue;
 
             var starName = parts[0];
-            if (!double.TryParse(parts[1], NumberStyles.Float, CultureInfo.InvariantCulture, out double x) ||
-                !double.TryParse(parts[2], NumberStyles.Float, CultureInfo.InvariantCulture, out double y) ||
-                !double.TryParse(parts[3], NumberStyles.Float, CultureInfo.InvariantCulture, out double z))
+            if (!double.TryParse(parts[1], NumberStyles.Float, CultureInfo.InvariantCulture, out var x) ||
+                !double.TryParse(parts[2], NumberStyles.Float, CultureInfo.InvariantCulture, out var y) ||
+                !double.TryParse(parts[3], NumberStyles.Float, CultureInfo.InvariantCulture, out var z))
             {
                 continue;
             }
 
-            var distance = GetDistance(xCoord, yCoord, zCoord, x, y, z);
-            maxHeap.Add(new StarData(distance, starName, x, y, z));
+            var squaredDistance = GetSquaredDistance(xCoord, yCoord, zCoord, x, y, z);
+
+            maxHeap.Enqueue(new StarData(squaredDistance, starName, x, y, z), -squaredDistance);
 
             if (maxHeap.Count > starsToReturn)
             {
-                maxHeap.Remove(maxHeap.First()); // Remove the farthest star
+                maxHeap.Dequeue();
             }
         }
 
-        return maxHeap.OrderBy(s => s.Distance)
+        var closestStars = new List<StarData>();
+
+        while (maxHeap.Count > 0)
+        {
+            closestStars.Add(maxHeap.Dequeue());
+        }
+
+        return closestStars
+            .OrderBy(s => s.SquaredDistance)
             .Select(s => (s.StarName, s.X, s.Y, s.Z))
             .ToList();
     }
 
-    // Compute Euclidean distance
-    private static double GetDistance(double x1, double y1, double z1, double x2, double y2, double z2)
+    private static double GetSquaredDistance(double x1, double y1, double z1, double x2, double y2, double z2)
     {
-        return Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
-    }
-}
-
-public class StarComparer : IComparer<StarData>
-{
-    public int Compare(StarData a, StarData b)
-    {
-        var distanceComparison = b.Distance.CompareTo(a.Distance);
-        return distanceComparison != 0 ? distanceComparison : string.Compare(a.StarName, b.StarName, StringComparison.Ordinal);
+        return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2);
     }
 }
